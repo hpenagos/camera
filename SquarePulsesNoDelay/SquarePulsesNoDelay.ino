6a85d6dd80e456a0f2db90039815fd3dc5127750
hpenagos <penagos@mit.edu>
2016-06-07 17:18:46 -0400

    Arduino required the file to be within a directory with a matching name

---
+++ SquarePulsesNoDelay/SquarePulsesNoDelay.ino
@@ -0,0 +1,93 @@
+/*
+Square pulses to drive PointGrey cameras in Rat1
+ */
+
+int ledPIN = 11;    
+int inPIN = 7;
+
+boolean triggered = false;
+unsigned long pulseTimer = 0;
+unsigned long pulseDur = 1;
+unsigned long pulsePer = 32;
+
+int buttonState;
+int lastButtonState = HIGH;
+boolean startRec = false;
+
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPIN,OUTPUT);
+  pinMode(inPIN,INPUT);
+}
+
+void loop() {
+  pulse(pulseDur,pulsePer);
+  buttonStuff();
+}
+
+void pulse(unsigned long duration, unsigned long period) {
+  
+  int pulseState;
+  unsigned long timekeeper = tElapsed(pulseTimer);
+  
+  if (triggered == false) {
+    triggered = true;
+    pulseState = HIGH;
+    pulseTimer = millis();
+  }
+  
+  else {
+    
+    if (startRec == true) {
+      triggered = false;
+      unsigned long newperiod;
+      
+      if (timekeeper <= 20) {newperiod = 20-timekeeper;}
+      else {newperiod = pulsePer;}
+      pulse(duration,newperiod);
+    }
+    
+    else {
+      if (timekeeper >= duration) {
+        if (timekeeper < period) {
+          digitalWrite(ledPIN,LOW);
+        }
+        else {
+          triggered = false;
+        }
+      }
+    }
+  }
+  digitalWrite(ledPIN,pulseState);
+}
+
+unsigned long tElapsed(unsigned long timer) {
+  unsigned long timediff;
+  timediff = millis()-timer;
+  return timediff;
+}
+
+void buttonStuff() {
+  int reading = digitalRead(inPIN);
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+  if (tElapsed(lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+      
+      if (buttonState == LOW) {
+        startRec = true;
+        /*pulse(pulseDur,4);*/
+        Serial.print("button pressed");
+      }
+      else {
+        startRec = false;
+      }
+    }
+  }
+  lastButtonState = reading;
+}
